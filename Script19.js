621b6938a82456b3651ff5b86ad92cf418a25e12
Mayurimk98 <mayurimkatwe@gmail.com>
2023-06-01 09:30:24 +0530

    1 june

---
+++ Script19.js
@@ -0,0 +1,69 @@
+
+//let name1 = "minskole"
+
+// for(let i=0;i<name1.length;i++){
+//     //console.log(i)
+//     console.log(name1[i])
+// }
+
+// for(let i=name1.length-1;i>=0;i--){
+//     console.log(name1[i])
+// }
+
+
+//eloksnim
+// let str = "" //m //im //nim
+// for (let i = 0; i < name1.length; i++) {
+//     str = name1[i] + str
+//     //m
+//     //i+m => im
+//     //n+im=> nim
+//     //s+nim=> snim
+//     //k+snim => ksnim==> eloksnim
+// }
+// console.log(str)
+
+//minskole
+
+// let str = ''
+// for (let i = 0; i < name1.length; i++) {
+//     str = str + name1[i]
+// }
+// console.log(str)
+
+//======================================================
+//shreeGanesh
+//count number of e
+
+// let student = "shreeGanesheeeyuuee"
+// let count = 0
+// for (let i = 0; i < student.length; i++) {
+//     // console.log(student[i])
+
+//     if (student[i] == 'e') {
+//         //count++
+//         count = count + 1
+//     }
+// }
+// console.log(count)
+
+//======================================
+let a = "mayuriRaoyeuuwiop"
+//5 vowel
+
+//a e i o u
+let count = 0
+for (let i = 0; i < a.length; i++) {
+    if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u') {
+        count++
+    }
+}
+console.log(count)
+
+//Hw ==> 
+
+
+
+
+
+
