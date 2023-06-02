844c7407060660cd959e1ba6c22c1f331cd35ca0
Mayurimk98 <mayurimkatwe@gmail.com>
2023-06-02 09:39:12 +0530

    2 june

---
+++ Script20.js
@@ -0,0 +1,120 @@
+
+
+//Array
+//String
+
+// let studentInfo = ["mayuri", "katwe", 25, 29]
+//                    0        1       2   3
+//add
+// studentInfo.push("vaibhav")
+// studentInfo.unshift("jaya")
+
+// delete
+// studentInfo.pop()
+// studentInfo.shift()
+
+// studentInfo.splice(1,3)
+
+//update
+// studentInfo[0]="vaibhav"
+// console.log(studentInfo)
+
+//retive/fetch
+// console.log(studentInfo[0])
+// console.log(studentInfo[3])
+
+let studentInfo = ["mayuri", "katwe", 25, 29]
+
+
+//indexwise
+
+//firstName
+//lastName
+//age
+//rollNumber
+
+//Object
+//let studInfo=[]
+
+//keys and value pair
+
+
+//keys :values => property
+// let info = {
+//     firstName: "Mayuri",
+//     lastName: "Katwe",
+//     age: 25,
+//     rollNumber: 29
+// }
+
+//add /create
+//delete
+//update 
+//retrive
+
+//CURD
+
+//1. add/create
+//dot notation
+// info.city="Nashik"
+// console.log(info)
+
+// //bracket notation
+// info['skill']="javascript"
+// console.log(info)
+
+//2. Update
+//dot notation
+// info.firstName="Vaibhav"
+// console.log(info)
+
+// info.rollNumber=200
+// console.log(info)
+
+// //bracket notation
+// info['lastName']="Pawar"
+// console.log(info)
+
+// info['age']=40
+// console.log(info)
+
+//3. retive/fetch
+//dot notation
+// console.log(info.firstName)//Mayuri
+// console.log(info.rollNumber)
+
+// //bracket notation
+// console.log(info["age"])
+// console.log(info['lastName'])
+
+
+//4. delete
+
+//dot nottaion
+
+// delete info.lastName
+// console.log(info)
+
+// //bracket nottaion
+// delete info['firstName']
+// console.log(info)
+
+let info = {
+    firstName: "Mayuri",
+    lastName: "Katwe",
+    age: 25,
+    rollNumber: 29,
+    firstName:"Vaibhav"
+}
+info.city="pune"
+console.log(info)
+
+
+
+
+
+
+
+
+
+
