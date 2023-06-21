45d4d466bbc8b68e0706c9a1bcd8436c0558e6c8
Mayurimk98 <mayurimkatwe@gmail.com>
2023-06-21 09:26:32 +0530

    21 june

---
+++ Script30.js
@@ -0,0 +1,71 @@
+
+//set ==> datatype
+
+//collection of unique value
+
+// let mySet=new Set()
+// console.log(mySet)
+
+// //add values in empty set
+
+// //1. add()
+// mySet.add("mayuri")
+// console.log(mySet)
+// mySet.add("vaibhav")
+// console.log(mySet)
+// mySet.add("prashant")
+// console.log(mySet)
+// mySet.add("kumar")
+// console.log(mySet)
+
+// //add the values in set with the help of set constructor
+// let mySet2=new Set(["a","b","c","d"])
+// console.log(mySet2)
+
+//2. delete()
+// console.log(mySet2.delete('z'))
+// console.log(mySet2)
+
+// console.log(mySet2.delete('a'))
+// console.log(mySet2)
+
+//3. has()
+// console.log(mySet2.has('a'))//false
+// console.log(mySet2.has('d'))
+
+//4. forEach()
+// mySet2.forEach(function(item){
+//     console.log(item)
+// })
+
+//5. clear()
+// mySet2.clear()
+// console.log(mySet2)
+
+//loop
+// for(let value of mySet2){
+//     console.log(value)
+// }
+
+
+//iterator
+
+let mySet2 = new Set(["a", "b", "c", "d"])
+console.log(mySet2)
+
+let r1 = mySet2.keys()
+console.log(r1)
+console.log(r1.next().value)
+console.log(r1.next().value)
+console.log(r1.next().value)
+console.log(r1.next().value)
+
+
+
+
+
+
+
+
+
+
